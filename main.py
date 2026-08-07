848f13f49ecc4661567f51fe5b055dac89ff89fe
Fatima <fatimaiboubkarne@gmail.com>
2026-08-07 17:21:56 +0100

    Stage 2: read endpoints with 404

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+
+memory=[
+    {
+        "id": 1,
+        "title": "Task nbr1",
+        "done": True
+    },
+    {
+        "id": 2,
+        "title": "Task nbr2",
+        "done": False
+    },
+    {
+        "id": 3,
+        "title": "Task nbr3",
+        "done": False
+    }
+]
 
 @app.get("/")
 def hello():
@@ -11,3 +30,19 @@ def hello():
 @app.get("/health")
 def health():
     return { "status": "ok" }
+
+
+@app.get("/tasks")
+def get_tasks():
+    return memory
+
+@app.get("/tasks/{id}")
+def get_tasks_by_id(id: int):
+    for task in memory :
+        if task["id"]==id :
+            return task
+
+    return JSONResponse(
+        status_code=404,
+        content={"error": f"Task {id} not found"}
+    )
